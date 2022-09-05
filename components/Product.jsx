8797248d478e1801d1dfa76f6458eb3ec92961dd
Firefly0 <agrigoroaei.ionut@gmail.com>
2022-09-05 12:50:54 +0300

    fix issue

--- components/Product.jsx
+++ components/Product.jsx
@@ -4,19 +4,22 @@ import Link from "next/link";
 import { urlFor } from "../lib/client";
 import { useStateContext } from "../context/StateContext";
 
-const Product = ({ product: { image, name, slug, price } }) => {
+const Product = ({ product: { image, name, slug, price } }) =>
+{
   const { setQty } = useStateContext();
 
   return (
     <div>
       <Link href={`/product/${slug.current}`}>
         <div className="product-card" onClick={() => setQty(1)}>
-          <img
-            src={urlFor(image && image[0])}
-            width={250}
-            height={250}
-            className="product-image"
-          />
+          {image &&
+            <img
+              src={urlFor(image && image[0])}
+              width={250}
+              height={250}
+              className="product-image"
+            />
+          }
           <p className="product-name">{name}</p>
           <p className="product-price">{price} RON</p>
         </div>
